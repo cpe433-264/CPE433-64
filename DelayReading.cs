8a38afe751edf4eaeaaa73c2172e8412fa2bcfb4
startg2545 <74952330+startg2545@users.noreply.github.com>
2021-12-17 18:10:22 +0700

    Add files via upload
    add an information

---
+++ DelayReading.cs
@@ -0,0 +1,25 @@
+using System;
+namespace DNWS
+{
+    class clientIP
+    {
+        public int v { get; set; }
+    }
+    class clientPort
+    {
+        public int v { get; set; }
+    }
+    class BrowserInfo
+    {
+        public string x { get; set; }
+    }
+    class AcceptLanguage
+    {
+        public string l { get; set; }
+    }
+    class AcceptEncoding
+    {
+        public string e { get; set; }
+    }
+
+}
