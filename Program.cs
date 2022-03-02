82c09849c55d5f26c9c35ff21c5998c022accf29
immarchz <46131013+immarchz@users.noreply.github.com>
2022-03-02 13:55:44 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -294,13 +294,8 @@ namespace DNWS
                     // Wait for client
                     clientSocket = serverSocket.Accept();
                     // Get one, show some info
-                    //_parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
-                    HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
-                    //hp.Process(); //move for thread to start
-                    
-                     // Implement Threads
-                    Thread t = new Thread(new ThreadStart(hp.Process)); //use hp.Process when thread start
-                    t.Start();
+                    _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
+                   
                 }
                 catch (Exception ex)
                 {
