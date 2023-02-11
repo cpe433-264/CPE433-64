a0e159bc169b755358d286d77e723db7e8207457
isne919 <paripat_y@cmu.ac.th>
2023-02-11 13:59:21 +0700

    homework2

--- Program.cs
+++ Program.cs
@@ -296,6 +296,8 @@ namespace DNWS
                     // Get one, show some info
                     _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
+                    Thread addThread = new Thread(new ThreadStart(hp.Process));
+                    addThread.Start();
                     hp.Process();
                 }
                 catch (Exception ex)
