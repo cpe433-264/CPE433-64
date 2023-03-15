b806101777e9a5e987da99158a5e8184b4637854
Neeranuch_See <neeranuch_see@cmu.ac.th>
2023-03-15 23:40:24 +0700

    got three greens for both website

--- PM25Plugin.cs
+++ PM25Plugin.cs
@@ -51,13 +51,6 @@ namespace DNWS
             }
 
             HTTPResponse response = null;
-            // StringBuilder sb = new StringBuilder();
-            // sb.Append("<html><body>");
-            // sb.Append("Location : " + pm25reading.data.city.name + "<br />");
-            // sb.Append("Date/Time : " + pm25reading.data.time.iso + "<br />");
-            // sb.Append("PM25 : " + pm25reading.data.iaqi.pm25.v + "<br />");
-            // sb.Append("PM10 : " + pm25reading.data.iaqi.pm10.v + "<br />");
-            // sb.Append("</body></html>");
             response = new HTTPResponse(200);
             response.body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize<PM25Reading>(pm25reading));
             return response;
